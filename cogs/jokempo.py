306dec73d2853fbe5cbb3d85d960b5ad4c567bfe
EduKobaiashi <edukobaiashi@gmail.com>
2021-06-06 06:53:46 -0300

    mudancas minimas no jokempo

--- cogs/jokempo.py
+++ cogs/jokempo.py
@@ -16,20 +16,20 @@ class Jokenpo(commands.Cog):
         print("Módulo Jokenpo carregado...")
 
     @commands.command(
-        aliases=["jokempo"],
+        aliases=["jokenpo"],
         brief="- help ajuda tutorial -",
         description="Desafia um jogador para pedra papel de tesoura"
     )
     @commands.guild_only()
     @commands.max_concurrency(1, per=commands.BucketType.user, wait=False)
     @is_blacklisted()
-    async def jokenpo(self, ctx, modo: typing.Union[discord.Member, str] = ""):
+    async def jokempo(self, ctx, modo: typing.Union[discord.Member, str] = ""):
         if len(ctx.message.mentions) == 0:
             if modo in ["help", "ajuda", "tutorial"]:
                 prefix = ctx.bot.cluster[str(ctx.guild.id)]["config"].find_one({"_id":"config_servidor"})["prefix"]
-                help_embed = discord.Embed(title="Como jogar Jokenpo")
+                help_embed = discord.Embed(title="Como jogar Jokempo")
                 help_embed.set_thumbnail(url="https://lh3.googleusercontent.com/cDo7huS5WTjjioOU2ds9t_KBwZ8wz9ttrlKfaafvBuzTkLAIM0jZNbVGXvL4QraXgefS=s150-rw")
-                help_embed.add_field(name="Comando", value=f"```Para jogar, utilize o comando {prefix}jokenpo @User, mencionando o usuário que deseja jogar contra.\n\nO usuário desafiado deve aceitar o desafio selecionando ✅.\n\nO bot então irá mandar uma mensagem privada para cada jogador escolher sua jogada.\n\nApós ambos jogadores escolherem sua jogada, o resultado será atualizado na mensagem original do bot.```")
+                help_embed.add_field(name="Comando", value=f"```Para jogar, utilize o comando {prefix}jokempo @User, mencionando o usuário que deseja jogar contra.\n\nO usuário desafiado deve aceitar o desafio selecionando ✅.\n\nO bot então irá mandar uma mensagem privada para cada jogador escolher sua jogada.\n\nApós ambos jogadores escolherem sua jogada, o resultado será atualizado na mensagem original do bot.```")
                 await ctx.send(embed=help_embed)
             elif modo == "":
                 await ctx.message.delete()
@@ -49,13 +49,9 @@ class Jokenpo(commands.Cog):
         else:
             embed_servidor = discord.Embed()
             embed_servidor.colour = 11869215
-            embed_servidor.set_author(name="Jokenpo", icon_url="https://lh3.googleusercontent.com/cDo7huS5WTjjioOU2ds9t_KBwZ8wz9ttrlKfaafvBuzTkLAIM0jZNbVGXvL4QraXgefS=s150-rw")
-            #embed_servidor.set_thumbnail(url="https://lh3.googleusercontent.com/cDo7huS5WTjjioOU2ds9t_KBwZ8wz9ttrlKfaafvBuzTkLAIM0jZNbVGXvL4QraXgefS=s150-rw")
+            embed_servidor.set_author(name="Jokempo", icon_url="https://lh3.googleusercontent.com/cDo7huS5WTjjioOU2ds9t_KBwZ8wz9ttrlKfaafvBuzTkLAIM0jZNbVGXvL4QraXgefS=s150-rw")
             embed_servidor.set_footer(text=f"{ctx.author.name} vs {modo.name}")
             embed_servidor.add_field(name="Aceitar desafio", value="```✅ para aceitar o desafio\n🚪 para recusar o desafio```", inline=False)
-            #embed_servidor.title = "Aceitar desafio"
-            #embed_servidor.description = "```✅ para aceitar o desafio\n🚪 para recusar o desafio```"
-            #embed_servidor.add_field(name="Aceitar desafio", value="```✅ para aceitar o desafio\n🚪 para recusar o desafio```", inline=False)
             menu_servidor = await ctx.send(embed=embed_servidor)
             await menu_servidor.add_reaction("✅")
             await menu_servidor.add_reaction("🚪")
@@ -79,9 +75,6 @@ class Jokenpo(commands.Cog):
                     embed_jogo = discord.Embed()
                     embed_jogo.set_footer(text=f"{ctx.author.name} vs {modo.name}")
                     embed_jogo.add_field(name="Sua jogada", value="✊ - Pedra\n✋ - Papel\n✌️ - Tesoura", inline=False)
-                    #embed_jogo.title = "Sua jogada"
-                    #embed_jogo.description = "✊ - Pedra\n✋ - Papel\n✌️ - Tesoura"
-                    #embed_jogo.add_field(name="Sua jogada", value="```✊ - Pedra\n✋ - Papel\n✌️ - Tesoura```")
                     try:
                         embed_jogo.set_author(name="Jokenpo", icon_url=ctx.author.avatar_url)
                         menu_p1 = await ctx.author.send(embed=embed_jogo)
@@ -101,9 +94,6 @@ class Jokenpo(commands.Cog):
                         return
 
                     embed_servidor.set_field_at(0, name="Desafio aceito", value="```Aguardando jogadas```", inline=False)
-                    #embed_servidor.title = "Desafio aceito"
-                    #embed_servidor.description = "Aguardando jogadas"
-                    #embed_servidor.set_field_at(0, name="Desafio aceito", value="```Aguardando jogadas```")
                     await menu_servidor.edit(embed=embed_servidor)
                     await menu_servidor.clear_reactions()
 
@@ -155,26 +145,21 @@ class Jokenpo(commands.Cog):
                         embed_servidor.remove_field(0)
                         if emoji_p1 == None:
                             embed_servidor.title = f"{modo.name} venceu a partida!"
-                            #embed_servidor.description = f"{ctx.author.name} não escolheu uma jogada e perdeu de WO"
                             embed_servidor.add_field(name=f"{modo.name} jogou:", value=f"{emoji_p2}", inline=False)
                             embed_servidor.add_field(name=f"{ctx.author.name} não jogou:", value=f"WO", inline=False)
                         elif emoji_p2 == None:
                             embed_servidor.title = f"{ctx.author.name} venceu a partida!"
-                            #embed_servidor.description = f"{modo.name} não escolheu uma jogada e perdeu de WO"
                             embed_servidor.add_field(name=f"{ctx.author.name} jogou:", value=f"{emoji_p1}", inline=False)
                             embed_servidor.add_field(name=f"{modo.name} não jogou:", value=f"WO", inline=False)
                         elif emoji_p1 == emoji_p2:
                             embed_servidor.title = "Empate!"
-                            #embed_servidor.description = f"Ambos jogadores jogaram {emoji_p1}"
                             embed_servidor.add_field(name=f"Ambos jogaram:", value=f"{emoji_p1}", inline=False)
                         elif (emoji_p1 == "✊" and emoji_p2 == "✌️") or (emoji_p1 == "✋" and emoji_p2 == "✊") or (emoji_p1 == "✌️" and emoji_p2 == "✋"):
                             embed_servidor.title = f"{ctx.author.name} venceu!"
-                            #embed_servidor.description = f"{emoji_p1} vs {emoji_p2}"
                             embed_servidor.add_field(name=f"{ctx.author.name} jogou:", value=f"{emoji_p1}", inline=False)
                             embed_servidor.add_field(name=f"{modo.name} jogou:", value=f"{emoji_p2}", inline=False)
                         else:
                             embed_servidor.title = f"{modo.name} venceu!"
-                            #embed_servidor.description = f"{emoji_p2} vs {emoji_p1}"
                             embed_servidor.add_field(name=f"{ctx.author.name} jogou:", value=f"{emoji_p1}", inline=False)
                             embed_servidor.add_field(name=f"{modo.name} jogou:", value=f"{emoji_p2}", inline=False)
 
